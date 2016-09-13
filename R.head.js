d192d284e9208b557e02f029835c2acc6ed78794
jon-wade <jon.wade@bulkpowders.com>
2016-09-13 09:57:19 +0100

    add in R.head example

---
+++ R.head.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const R = require('ramda')
+
+/*
+returns the first element of an array or first character of a string
+ */
+console.log(R.head([1, 2, 3]))
+console.log(R.head('123'))
+
+//logs 1
+//logs 1
+
+
+
